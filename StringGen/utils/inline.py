fdce84aa00d25f96db297b26e6e156452ae4d48d
ZenxDark <142667006+OnDrugs088@users.noreply.github.com>
2023-08-28 10:51:35 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://t.me/DARKBOTS088/2"
+                text="sᴏᴜʀᴄᴇ", url="https://github.com/OnDrugs088/StringGenBot"
             ),
         ],
     ]
